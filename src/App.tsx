a0aab995ccf7cc29ce5f8cb3454c2a6cb82bd524
[dyad] <sanjayofficial720@gmail.com>
2026-04-02 12:55:30 +0530

    [dyad] Fixed AddMedicine import error - wrote 4 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import AddMedicine from "./pages/AddMedicine"; // Fixed import path
+import AddMedicine from "./pages/AddMedicine";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
