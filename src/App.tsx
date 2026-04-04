711f74705296beb66f6d63e8556d317c0f4a26ef
[dyad] <sanjayofficial720@gmail.com>
2026-04-04 13:07:35 +0530

    [dyad] Fixed TypeScript errors by renaming History component to MedicationHistory and updating imports - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";  
-import Dashboard from "./pages/Dashboard";  
-import Index from "./pages/Index";  
-import Login from "./pages/Login";  
-import SignUp from "./pages/SignUp";  
-import NotFound from "./pages/NotFound";  
-import BottomTabBar from "./components/BottomTabBar";  
-import AIButton from "./components/AIButton";  
-import AddMedicine from "./pages/AddMedicine";  
-import History from "./pages/History";  
-import FamilyMembers from "./pages/FamilyMembers";  
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
+import BottomTabBar from "./components/BottomTabBar";
+import AIButton from "./components/AIButton";
+import AddMedicine from "./pages/AddMedicine";
+import MedicationHistory from "./pages/MedicationHistory";
+import FamilyMembers from "./pages/FamilyMembers";
 
-const App = () => {  
-  return (  
-    <BrowserRouter>  
-      <Routes>  
-        <Route path="/" element={<Index />} />  
-        <Route path="/login" element={<Login />} />  
-        <Route path="/signup" element={<SignUp />} />  
-        <Route path="/dashboard" element={<Dashboard />} />  
-        <Route path="/add-medicine" element={<AddMedicine />} />  
-        <Route path="/history" element={<History />} />  
-        <Route path="/family-members" element={<FamilyMembers />} />  
-        <Route path="*" element={<NotFound />} />  
-      </Routes>  
-    </BrowserRouter>  
-  );  
-};  
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/add-medicine" element={<AddMedicine />} />
+        <Route path="/history" element={<MedicationHistory />} />
+        <Route path="/family-members" element={<FamilyMembers />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
